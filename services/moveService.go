bd13d9d72b992fc4d25acdc81b7289a7d3447a2d
tidarjoey <tidarjoey@live.com>
2024-01-27 16:33:01 +0700

    Init move service

---
+++ services/moveService.go
@@ -0,0 +1,16 @@
+package service
+
+import (
+	"fmt"
+	"lab-go-chess-engine/models"
+)
+
+func GetLegalMovesForKing(board *Board, position Position) []Move {
+	var moves []Move
+	// Calculate king's legal moves from 'position'
+	return moves
+}
+
+func GetLegalMovesForQueen(board *Board, position Position) []Move {
+	// Similar implementation for queen
+}
